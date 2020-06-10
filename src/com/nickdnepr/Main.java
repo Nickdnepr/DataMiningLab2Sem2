7903735aa843551c7f7115c9856956ec05d7cd57
Nick_dnepr <nick.dnepr@gmail.com>
2020-06-10 07:18:16 +0300

    -- added anova

--- src/com/nickdnepr/Main.java
+++ src/com/nickdnepr/Main.java
@@ -6,6 +6,7 @@ import com.nickdnepr.core.analysis.CharacteristicsFilter;
 import com.nickdnepr.core.cluster_builders.HierarchicalClusterBuilder;
 import com.nickdnepr.core.Item;
 import com.nickdnepr.core.cluster_builders.KMeansClusterBuilder;
+import com.nickdnepr.utils.FTable;
 import com.nickdnepr.utils.FileParser;
 
 import java.io.IOException;
@@ -14,9 +15,13 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+//        System.out.println(FTable.getValue(3,1));
+//        System.exit(0);
         ArrayList<Item> items = FileParser.readFile("file");
 //        CharacteristicsFilter.calculateMatrix(items);
-        CharacteristicsFilter.removeUnnecessaryCharacteristics(items);
+        CharacteristicsFilter.removeUnnecessaryCharacteristicsCorrelation(items);
+        System.out.println();
+        CharacteristicsFilter.calculateFValues(items);
         System.exit(0);
         for (Item item : items) {
             System.out.println(item.toLongString());
--- src/com/nickdnepr/core/analysis/CharacteristicsFilter.java
+++ src/com/nickdnepr/core/analysis/CharacteristicsFilter.java
@@ -1,13 +1,16 @@
 package com.nickdnepr.core.analysis;
 
 import com.nickdnepr.core.Item;
+import com.nickdnepr.utils.FTable;
 import com.nickdnepr.utils.Pair;
 
 import java.util.*;
 
 public class CharacteristicsFilter {
 
-    public static ArrayList<Item> removeUnnecessaryCharacteristics(ArrayList<Item> items) {
+    // correlation
+
+    public static ArrayList<Item> removeUnnecessaryCharacteristicsCorrelation(ArrayList<Item> items) {
         ArrayList<Item> newItems = new ArrayList<>();
 
         Pair<ArrayList<String>, double[][]> processingResult = calculateMatrix(items);
@@ -36,7 +39,7 @@ public class CharacteristicsFilter {
                 }
             }
         }
-        for (Item item:items){
+        for (Item item : items) {
             newItems.add(item.newCharacteristics(processed));
         }
 
@@ -44,13 +47,8 @@ public class CharacteristicsFilter {
     }
 
     public static Pair<ArrayList<String>, double[][]> calculateMatrix(ArrayList<Item> items) {
-        HashMap<String, ArrayList<Double>> characteristics = new LinkedHashMap<>();
-        for (Item item : items) {
-            for (String characteristic : item.getCharacteristics()) {
-                characteristics.putIfAbsent(characteristic, new ArrayList<>());
-                characteristics.get(characteristic).add(item.getCharacteristic(characteristic));
-            }
-        }
+        HashMap<String, ArrayList<Double>> characteristics = getCharacteristicsFromItems(items);
+
         double[][] coefficientMatrix = new double[characteristics.size()][characteristics.size()];
         ArrayList<String> orderedCharacteristics = new ArrayList<>(characteristics.keySet());
 //        for (String characteristic1 : orderedCharacteristics) {
@@ -59,7 +57,6 @@ public class CharacteristicsFilter {
 //                System.out.println(characteristic1 + " and " + characteristic2 + " correlation: " + coefficientMatrix[orderedCharacteristics.indexOf(characteristic1)][orderedCharacteristics.indexOf(characteristic1)]);
 //            }
 //        }
-
 
 
         for (int i = 0; i < orderedCharacteristics.size(); i++) {
@@ -104,5 +101,67 @@ public class CharacteristicsFilter {
         }
         denominator = Math.sqrt(denominatorFirstSum * denominatorSecondSum);
         return denominator == 0 ? 0.0 : numerator / denominator;
+    }
+
+    // variance
+
+    public static void calculateFValues(ArrayList<Item> items) {
+        HashMap<String, ArrayList<Double>> characteristics = getCharacteristicsFromItems(items);
+        HashMap<String, Double> means = new HashMap<>();
+        ArrayList<Double> allValues = new ArrayList<>();
+        for (String ch : characteristics.keySet()) {
+            means.put(ch, getMean(characteristics.get(ch)));
+            allValues.addAll(characteristics.get(ch));
+        }
+        double totalMean = getMean(allValues);
+        double betweenGroupSquareSum = calculateSquareSum(new ArrayList<>(means.values()), totalMean) * items.size();
+        int dfb = characteristics.size() - 1;
+//        HashMap<String, Double> fValues = new HashMap<>();
+        for (String ch : characteristics.keySet()) {
+            double withinGroupSquareSum = calculateSquareSum(characteristics.get(ch));
+            int dfw = (items.size() - 1) * characteristics.size();
+            double f = (betweenGroupSquareSum / dfb) / (withinGroupSquareSum / dfw);
+            if (withinGroupSquareSum == 0) {
+                f = 0;
+            }
+            if (f > FTable.getValue(dfb, dfw)) {
+                System.out.println(ch + " is relevant");
+            } else {
+                System.out.println(ch + " is irrelevant");
+            }
+        }
+        return;
+    }
+
+    private static double calculateSquareSum(ArrayList<Double> values) {
+        return calculateSquareSum(values, getMean(values));
+    }
+
+    private static double calculateSquareSum(ArrayList<Double> values, double mean) {
+        double result = 0;
+        for (double value : values) {
+            result += Math.pow(value - mean, 2);
+        }
+        return result;
+    }
+
+    // common
+    private static HashMap<String, ArrayList<Double>> getCharacteristicsFromItems(ArrayList<Item> items) {
+        HashMap<String, ArrayList<Double>> characteristics = new LinkedHashMap<>();
+        for (Item item : items) {
+            for (String characteristic : item.getCharacteristics()) {
+                characteristics.putIfAbsent(characteristic, new ArrayList<>());
+                characteristics.get(characteristic).add(item.getCharacteristic(characteristic));
+            }
+        }
+        return characteristics;
+    }
+
+    private static double getMean(ArrayList<Double> values) {
+        double sum = 0;
+        for (Double d : values) {
+            sum += d;
+        }
+        return sum / values.size();
     }
 }
